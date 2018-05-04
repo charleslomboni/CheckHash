4c36235d6e812a774fea7777343b2f0ffc16e0ac
charleslomboni <charleslomboni@gmail.com>
2018-05-04 20:22:34 -0300

    :art: Update filterAv

--- CheckHash.py
+++ CheckHash.py
@@ -14,22 +14,29 @@ import argparse
 import requests
 import json
 import os
+import time
 
 def getArgs():
-    parser = argparse.ArgumentParser("verifyHash")
+    parser = argparse.ArgumentParser("CheckHash")
     parser.add_argument("-p", "--rootPath",
                         help="Path from the root folder")
+    parser.add_argument("-av","--filterAv",
+                        help="Verify if specific AV cath this malware")
     return parser.parse_args()
 
 def getMD5Hash(filePath):
-     if os.path.isfile(filePath):
-        hasher = hashlib.md5()
-        with open(filePath, 'rb') as afile:
-            buf = afile.read()
-            hasher.update(buf)
-        return hasher.hexdigest()
 
-def sendToVirusTotal(md5Hash):
+     if os.path.isfile(filePath):
+        extensionFile = os.path.splitext(filePath)[1]
+
+        if extensionFile.lower() != ".url":
+            hasher = hashlib.md5()
+            with open(filePath, 'rb') as afile:
+                buf = afile.read()
+                hasher.update(buf)
+            return hasher.hexdigest()
+
+def sendToVirusTotal(md5Hash, fileName):
     statusCode = ""
 
     try:
@@ -43,7 +50,7 @@ def sendToVirusTotal(md5Hash):
 
         return response.json()
     except:
-        print "Hash: " + md5Hash + " -- StatusCode: " + str(statusCode)
+        print "FileName => " + fileName + " :: Hash => " + md5Hash + " :: StatusCode => " + str(statusCode)
 
 
 def getItemsDirBy(rootDirPath):
@@ -55,12 +62,20 @@ def getItemsDirBy(rootDirPath):
 
     return foldersName
 
+
+def isAv(av, response):
+    scans = response.get("scans")
+    symantecAv = scans.get(av)
+    return symantecAv
+
 def main():
 
     args = getArgs()
 
     # return all folders in passed folder
     foldersName = getItemsDirBy(args.rootPath)
+
+    print "Analysis was started.."
 
     for folder in foldersName:
         # get all files inseide folder
@@ -76,15 +91,19 @@ def main():
                 md5Hash = getMD5Hash(fullPath)
 
                 if md5Hash:
-                    response = sendToVirusTotal(md5Hash)
+                    response = sendToVirusTotal(md5Hash, singleFile)
+
+                    # sleep time to bypass limit for virustotal
+                    time.sleep(5)
 
                     if response is None:
                         break
 
-                    if response.get("positives", "none") != "none"  and response.get("positives", "none") > 0:
-                        print "Artifact has been identified :("
-                    else:
-                        print "New artifact! Start the analysis NOW! \o/ \n" + fullPath
+                    if response.get("positives", "none") <= 0:
+                        print "** NEW ** [ " + fullPath + " ] :: Hash => " + md5Hash
+                        responseAv = isAv(args.filterAv, response)
+                        print args.filterAv + " detected => " + str(responseAv.get("detected"))
+
             except Exception as ex:
                print ex
     pass
